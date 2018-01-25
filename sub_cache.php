9ad712ceca1fc00dbf70d91852ac70e81fba8f66
Lastorder <lastorder@yjsoft.xyz>
2018-01-25 14:59:14 +0900

    Update sub_cache.php

--- sub_cache.php
+++ sub_cache.php
@@ -5,12 +5,12 @@ set_time_limit(0);
 $emotes_start = false;
 $emotes_body = false;
 $emotes_data = false;
-$emotes = array();
 $temp_emotes_data = array();
 $curkey = "";
+$curcode = "";
 
 function objStart($value, $property) {
-	global $emotes_start, $emotes_body, $emotes_data, $emotes, $temp_emotes_data, $curkey;
+	global $emotes_body, $emotes_data;
 	
 	if($emotes_body) {
 		$emotes_data = true;
@@ -18,17 +18,24 @@ function objStart($value, $property) {
 }
 
 function objEnd($value, $property) {
-	global $emotes_start, $emotes_body, $emotes_data, $emotes, $temp_emotes_data, $curkey;
+	global $emotes_start, $emotes_body, $emotes_data, $temp_emotes_data, $curkey, $curcode, $fp;
 	
 	if($emotes_body) {
 		$emotes_data = false;
-		$emotes[$temp_emotes_data["code"]] = $temp_emotes_data;
+		$emotes[$curcode] = $temp_emotes_data;
+		$output = json_encode($emotes);
+		$output = substr($output,1,strlen($output) - 2);
+		fwrite($fp, $output . ",");
+		//fclose($fp);
+		//$fp = fopen(__DIR__ . '/json/sub.json', 'a');
+		//if($fp === FALSE) die("File open error!\n");
 		$curkey = "";
+		$curcode = "";
 	}
 }
 
 function arrayStart($value, $property) {
-	global $emotes_start, $emotes_body, $emotes_data, $emotes, $temp_emotes_data, $curkey;
+	global $emotes_start, $emotes_body, $emotes_data, $temp_emotes_data, $curkey, $curcode;
 	
 	// emotes array has started
 	if($emotes_start) {
@@ -38,7 +45,7 @@ function arrayStart($value, $property) {
 }
 
 function arrayEnd($value, $property) {
-	global $emotes_start, $emotes_body, $emotes_data, $emotes, $temp_emotes_data, $curkey;
+	global $emotes_start, $emotes_body, $emotes_data, $temp_emotes_data, $curkey, $curcode;
 	
 	// emotes array has ended
 	if($emotes_body) {
@@ -49,7 +56,7 @@ function arrayEnd($value, $property) {
 }
 
 function property($value, $property) {
-	global $emotes_start, $emotes_body, $emotes_data, $emotes, $temp_emotes_data, $curkey;
+	global $emotes_start, $emotes_body, $emotes_data, $temp_emotes_data, $curkey, $curcode;
 	
 	if($value == "emotes") {
 		$emotes_start = true;
@@ -57,16 +64,18 @@ function property($value, $property) {
 		$emotes_data = false;
 	} else {
 		if($emotes_data) {
-			$curkey = $value;
+			if($value == "id" || $value == "code") $curkey = $value;
+			else $curkey = "SKIP";
 		}
 	}
 }
 
 function scalar($value, $property) {
-	global $emotes_start, $emotes_body, $emotes_data, $emotes, $temp_emotes_data, $curkey;
+	global $emotes_start, $emotes_body, $emotes_data, $temp_emotes_data, $curkey, $curcode;
 	
 	if($emotes_data) {
-		$temp_emotes_data[$curkey] = $value;
+		if($curkey == "id") $temp_emotes_data[$curkey] = (int) $value;
+		if($curkey == "code") $curcode = $value;
 	}
 }
 
@@ -77,16 +86,26 @@ $parser->setObjectHandlers('objStart', 'objEnd');
 $parser->setPropertyHandler('property');
 $parser->setScalarHandler('scalar');
 
-echo "<pre>Parsing sub emoticon json...\n";
+if(php_sapi_name() !== 'cli') echo "<pre>";
+
+$output = "{";
+$fp = fopen(__DIR__ . '/json/sub.json', 'w');
+if($fp === FALSE) die("File clear error!\n");
+fwrite($fp, $output);
+fclose($fp);
+echo "File clear done!\n";
+
+$fp = fopen(__DIR__ . '/json/sub.json', 'a');
+if($fp === FALSE) die("File open error!\n");
+
+echo "Parsing sub emoticon json...\n";
 $parser->parseDocument(__DIR__ . '/json/subscriber.json');
 echo "Parse done!\n";
-$output = json_encode($emotes);
 
-$fp = fopen(__DIR__ . '/json/sub.json', 'w');
-if($fp === FALSE) echo "File save error!\n";
-else {
-    fwrite($fp, $output);
-    fclose($fp);
-    echo "Save done!\n";
-}
-echo "</pre>";
+//$output = json_encode($emotes);
+$output = '"EOF_NULL_NA":{"id":0}}';
+fwrite($fp, $output);
+echo "Save done!\n";
+fclose($fp);
+
+if(php_sapi_name() !== 'cli') echo "</pre>";
